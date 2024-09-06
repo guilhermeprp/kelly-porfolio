c15feea8cc78e97f5e55e77dc3e6fe0cc06e42aa
Guilherme Pimenta <mirante-guilherme.pereira@ailos.coop.br>
2024-09-06 18:31:03 -0300

    fix: change img assets file format to avoid bugs

--- src/presentation/components/background.tsx
+++ src/presentation/components/background.tsx
@@ -2,10 +2,10 @@ import { getBase64Image } from "@/helpers/base64Image";
 import Image from "next/image";
 
 export default async function Background() {
-	const getBlurDataUrl = await getBase64Image(process.env.HOST_NAME + '/background.jfif');
+	const getBlurDataUrl = await getBase64Image(process.env.HOST_NAME + '/background.jpg');
 
 	return <Image
-		src="/background.jfif"
+		src="/background.jpg"
 		alt="background"
 		className="fixed inset-0 z-0 opacity-40 invert h-dvh w-dvw"
 		width={1920}
